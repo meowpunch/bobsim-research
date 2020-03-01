6a8e78883f993e793fa1d25f5741ac5a2ea85b4c
meowpunch <jinhoonbae@gmail.com>
2020-03-01 21:57:07 +0900

    refacoring for atomic design

--- simulator/main.py
+++ simulator/main.py
@@ -1,8 +1,16 @@
 import boto3
+from rds.handler import handler
 
-# Let's use Amazon S3
-s3 = boto3.resource('s3')
+if __name__ == '__main__':
 
-# Print out bucket names
-for bucket in s3.buckets.all():
-    print(bucket.name)
+    # Let's use Amazon S3
+    s3 = boto3.resource('s3')
+
+    # Print out bucket names
+    for bucket in s3.buckets.all():
+        print(bucket.name)
+
+    table_name = "Employee"
+    handler();
+
+
