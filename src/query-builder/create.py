5994ce12bdde05a53540c971c6c2925df75fde6d
Hankpkj <55626802+Hankpkj@users.noreply.github.com>
2020-03-05 11:54:18 +0900

    add create table.py

--- src/query-builder/create.py
+++ src/query-builder/create.py
@@ -1,8 +1,13 @@
+# create.py
 import sys
 import logging
-from src.constant.rds_config import *
+#from src.constant.rds_config import *
 import pymysql
-#rds settings
+
+# rds settings
+
+
+
 
 rds_host  = "production-bobsim-aurora-instance-1.cm9kakdihdlv.ap-northeast-2.rds.amazonaws.com"
 name = db_username
@@ -34,7 +39,6 @@ logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
 
 def create_table() :
     table_name = input('테이블 이름을 쓰시오 : ')
-    attribute()
     return table_name
 
 def att_num() :
@@ -42,14 +46,33 @@ def att_num() :
     return num
 
 def attribute(num):
-        att_name = list(input('{}번째 attribute 의 이름을 쓰세요 (없으면 0입력) :  '.format(num)))
+        att_name = input('{}번째 attribute 의 이름을 쓰세요  :  '.format(num+1))
         return att_name
+
 def datatype(a):
-    datatype = input('{}번째 attribute의 Data_Type을 입력하세요 : '.format(int(a)))
+    datatype = input('{}번째 attribute의 Data_Type을 입력하세요 : '.format(int(a)+1))
     return datatype
-def datacons(a):
-    constraint = input(' 나머지 조건을 입력해주세요 : ')
-    return constraint
+
+def datacons(num):
+    cons = input('{}번째 attribute의 나머지 조건을 입력해주세요(없을시 0 입력) : '.format(num+1))
+    if  cons == "0"  :
+        cons = ""
+        return cons
+    else :
+        return cons
+
+def foreign_key() :
+    foreign = input('FK키가 되는 attribute를 입력하세요 (2개 이상일 경우 ,로 구분  없을시 0 입력) : ')
+    if foreign == "0" :
+        foreign = ""
+        return foreign
+    else :
+        references_table = input(" reference table 이름을 입력해주세요 : ")
+        references_att= input(" reference attribute 이름을 입력해주세요 : ")
+    return [references_table, references_att]
+
+
+
 #def more_data:
   #  a= input()
 
@@ -57,21 +80,49 @@ def datacons(a):
   #      cur.execute("create ",table_name," ( ",)
 
 
-class CreatTable :
+def CreatTable():
     table_name= create_table()
-    att_number= int(att_num())
-    for n in att_number:
-        att_name=list(attribute(n))
-        att_type=list(datatype(n))
-        att_cons=list(datacons(n))
+    attNumber= int(att_num())
+    attName =  [0 for i in range(attNumber)]
+    attType= [0 for i in range(attNumber)]
+    attCons= [0 for i in range(attNumber)]
+    full_sentence = [0 for i in range(attNumber)]
+    for n in range(0,attNumber):
+        attName[n]=attribute(n)
+        attType[n]=datatype(n)
+        attCons[n]=datacons(n)
+        full_sentence[n]= attName[n]+ "  " + attType[n]+ "  " + attCons[n]
+        print(full_sentence[n])
+    foreignKey=foreign_key()
+    i = 0
+    full_full_sentence = ""
+    while i < len(full_sentence)-1:
+        full_full_sentence += str(full_sentence[i])
+        full_full_sentence += str(" , ")
+        i += 1
+        if i == len(full_sentence)-1:
+            full_full_sentence += str(full_sentence[i])
 
-    with conn.crsor() as cur:
-        sentence=[]
+    print('create table if not exists '+table_name+' ('+ full_full_sentence + " ) ")
+    with conn.cursor() as cur:
+          cur.execute('create table if not exists '+table_name+' ('+ full_full_sentence + " ) ")
+          conn.commit()
 
-        for n in att_num() :
-            sentence[n-1] = str("creat table ",
 
-            cur.excute("create table ",table_name," ( ",   )
+
+         #cur.execute("select * from recipe")
+
+
+CreatTable()
+
+
+#
+# with conn.cursor() as cur:
+#         sentence=[]
+#
+#     for n in att_num() :
+#             sentence[n-1] = str(
+#             cur.excute("create table ",table_name," ( ",   )
 
 
 #getData = {}
@@ -81,7 +132,7 @@ class CreatTable :
 # with conn.cursor() as cur:
 #         cur.execute(create_)
 
-create_table()
+# create_table()
 
 #with conn.cursor() as cur:
         # cur.execute("create table Employee ( EmpID  int NOT NULL, Name varchar(255) NOT NULL, PRIMARY KEY (EmpID))")
@@ -96,7 +147,7 @@ create_table()
         #     #print(row)
 #conn.commit()
 
-print ("Added %d items from RDS MySQL table" %(item_count))
+#print ("Added %d items from RDS MySQL table" %(item_count))
 
 
     # print("Added %d items from RDS MySQL table" % item_count)
