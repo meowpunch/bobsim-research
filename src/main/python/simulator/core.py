1e8fbf7ebfa0cf6e88e06fc81623a46bb19813ae
meowpunch <jinhoonbae@gmail.com>
2020-03-08 21:53:30 +0900

    add comments in simulator

--- src/main/python/simulator/core.py
+++ src/main/python/simulator/core.py
@@ -2,9 +2,13 @@ from util.db import load_query
 
 
 class Simulator:
+    """
+        user_num -> Assume the number of Active User
 
+    """
     def __init__(self):
         self.sql_filename = 'item_distribution.sql'
+        self.user_num = 1000
 
     def execute(self):
         return self.process()
@@ -15,7 +19,13 @@ class Simulator:
     def process(self):
         raw_data = self.load_data()
         """
-        TODO: logic comes here
+        TODO:
+            0. set the number of virtual active user
+           ----
+            1. quantify food materials
+            2. price food materials
+            3. find likely menu(recipe) by user's food materials.
+            4. calculate object function (opportunity cost)
         """
         print(raw_data)
 
