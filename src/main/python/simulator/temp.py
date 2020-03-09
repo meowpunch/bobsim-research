b699a9bfe1be75df90784740100a119790a31935
meowpunch <jinhoonbae@gmail.com>
2020-03-09 17:04:47 +0900

    create user class & detail process & write json in s3
    process:
      fabricate date -> play user -> save raw data

--- src/main/python/simulator/temp.py
+++ src/main/python/simulator/temp.py
@@ -1,22 +1,9 @@
 from simulator.core import Simulator
 
 
-def insert(name: str, sensitive: int, table):
-    +table +".sql"
-    path()
-    a = (name,sensitive)
-
-    print(type(a))
-    print(str(a))
-
 def main():
     simulator = Simulator()
     simulator.execute()
-
-    query = "INSERT INTO "
-
-    insert_item_table(name="경재", sensitive=0, table="item")
-
 
 
 if __name__ == '__main__':
