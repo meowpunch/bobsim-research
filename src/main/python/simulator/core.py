b2d6f0a69aca196082dd4e83d0ab4e59de0b6bfe
meowpunch <jinhoonbae@gmail.com>
2020-03-09 10:37:57 +0900

    apply load return & void query

--- src/main/python/simulator/core.py
+++ src/main/python/simulator/core.py
@@ -1,4 +1,4 @@
-from util.db import load_query
+from util.db import load_return_query
 
 
 class Simulator:
@@ -14,10 +14,13 @@ class Simulator:
         return self.process()
 
     def load_data(self):
-        return load_query(self.sql_filename)
+        return load_return_query(self.sql_filename)
 
     def process(self):
         raw_data = self.load_data()
+        print(type(raw_data))
+        for row in raw_data:
+            print(row)
         """
         TODO:
             0. set the number of virtual active user
