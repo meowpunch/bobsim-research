93da7bd8b5685f82c2e74f2c746da90baa7847d9
Hankpkj <55626802+Hankpkj@users.noreply.github.com>
2020-03-24 18:59:04 +0900

    HANK/MEOW: modify regression.py

--- src/main/python/model/regression.py
+++ src/main/python/model/regression.py
@@ -1,3 +1,4 @@
+from sklearn.linear_model import ElasticNet
 
 
 class RegressionModel:
@@ -5,12 +6,16 @@ class RegressionModel:
         TODO: Ridge, Lasso, Elastic.
     """
     def __init__(self, x_train, y_train):
+        self.model = ElasticNet()  # let's think about params
         self.x_train = x_train
         self.y_train = y_train
 
-    def fit(self):
-        pass
+    def fit(self, sample_weight=None):
+        self.model.fit(self.x_train, self.y_train, sample_weight)
 
     def predict(self, x_test):
-        pass
+        return self.model.predict(X=x_test)
+
+    def score(self):
+        return self.model.score(self.x_train, self.y_train)
 
