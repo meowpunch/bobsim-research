5988d3a0cef9bc849a84c41bdad1ad9f3bd3077d
Hankpkj <55626802+Hankpkj@users.noreply.github.com>
2020-04-03 17:53:33 +0900

    HANK/MEOW: create transform class

--- src/main/python/util/transform.py
+++ src/main/python/util/transform.py
@@ -1,36 +1,66 @@
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, FunctionTransformer, StandardScaler
 
 from data_pipeline.open_data_raw_material_price.core import OpenDataRawMaterialPrice
 from util.s3_manager.manager import S3Manager
+import numpy as np
 
 
 class Transformer:
-    def __init__(self, strategy=None):
-        self.strategy = {
-            "categorical_features": {
-                "one_hot_encoding": ['품목명', '조사지역명', 'is_weekend', 'season'],
-            },
-            "numerical_features": {
-                "log": [
-                    "최저기온(°C)", "최대 풍속(m/s)", "평균 풍속(m/s)", "최소 상대습도(pct)",
-                    "강수 계속시간(hr)", "평균 수온(°C)", "평균 최대 파고(m)", "평균 유의 파고(m)",
-                    "최고 유의 파고(m)", "최고 최대 파고(m)", "평균 파주기(sec)", "최고 파주기(sec)"
-                ],
-                'standard': ["당일조사가격"],
-                "none": [
-                    "평균기온(°C)", "최고기온(°C)", "일강수량(mm)",
-                    "평균 상대습도(pct)", "평균기압(hPa)", "평균 기온(°C)"
-                ]
+
+    def __init__(self, strategy=None, df=None):
+        """
+        TODO : overwrite to self.strategy
+        :param strategy:
+        """
+
+        if strategy is None:
+            self.strategy = {
+                "categorical_features": {
+                    "one_hot_encoding": ['품목명', '조사지역명', 'is_weekend', 'season'],
+                },
+                "numerical_features": {
+                    "log": [
+                        "최저기온(°C)", "최대 풍속(m/s)", "평균 풍속(m/s)", "최소 상대습도(pct)",
+                        "강수 계속시간(hr)", "평균 수온(°C)", "평균 최대 파고(m)", "평균 유의 파고(m)",
+                        "최고 유의 파고(m)", "최고 최대 파고(m)", "평균 파주기(sec)", "최고 파주기(sec)"
+                    ],
+                    "standard": ["당일조사가격"],
+                    "none": [
+                        "평균기온(°C)", "최고기온(°C)", "일강수량(mm)",
+                        "평균 상대습도(pct)", "평균기압(hPa)", "평균 기온(°C)"
+                    ]
+                }
             }
-        }
+        else:
+            self.strategy = strategy
+
+        self.df = df
+        # categorical columns
+        categorical_features = self.strategy["categorical_features"]["one_hot_encoding"]
+
+        # numerical columns
+        numeric_features_log = self.strategy["numerical_features"]["log"]
+        numeric_features_standard = self.strategy["numerical_features"]["standard"]
+        numeric_features_none = self.strategy["numerical_features"]["none"]
+
+
+
 
     def header(self, df):
         print(df["품목명"].unique())
         x = df[self.strategy["categorical_features"]["one_hot_encoding"]].apply(lambda x: x.unique(), axis=1)
         print(x)
 
+    def execute(self):
+        return self.proccess()
+
     def proccess(self):
-        pass
+        transformed_df = self.column_transformer.fit_transform(self.df)
+        return transformed_df
 
 
 def load(filename):
@@ -49,8 +79,9 @@ def load(filename):
 
 def main():
     df = load("201908")
-    t = Transformer()
-    t.header(df)
+    t = Transformer(df=df)
+    print(t.proccess())
+    # t.header(df)
 
 
 if __name__ == '__main__':
