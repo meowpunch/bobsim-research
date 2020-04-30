78482b51955756ad03de48e855dff916903907d4
meowpunch <jinhoonbae@gmail.com>
2020-04-30 20:14:00 +0900

    MEOW: refactoring & metric by other term

--- src/main/python/analysis/food_material_price/main.py
+++ src/main/python/analysis/food_material_price/main.py
@@ -1,26 +1,13 @@
-from analysis.food_material_price.research import metric_by_test_size, metric_by_other_day
+from analysis.food_material_price.research import metric_by_test_size, metric_by_other_term
 from util.build_dataset import build_master
 
 
-def research_metric_by_test_size():
-    dataset = build_master(bucket_name="production-bobsim", dataset="process_fmp", date="201908", pipe_data=False)
-
-    metric_by_test_size(
-        df=dataset, test_sizes=range(1, 10), train_size=10
-    )
-
-
-def research_metric_by_one_day():
-    dataset = build_master(bucket_name="production-bobsim", dataset="process_fmp", date="201908", pipe_data=False)
-
-    metric_by_other_day(
-        df=dataset, train_size=5, test_size=1
-    )
-
-
 def main():
-    # research_metric_by_test_size()
-    research_metric_by_one_day()
+    dataset = build_master(bucket_name="production-bobsim", dataset="process_fmp", date="201908", pipe_data=False)
+
+    # metric_by_test_size(df=dataset, test_sizes=range(1, 10), train_size=5)
+
+    metric_by_other_term(df=dataset, train_size=10, test_size=2, n_days=range(8))
     pass
 
 
--- src/main/python/util/visualize.py
+++ src/main/python/util/visualize.py
@@ -24,6 +24,16 @@ def plot(data: list):
     plt.show()
 
 
+def series_plot(ser: pd.Series, kind="bar", y_label="y", x_label="x", title="Untitled", d=0.1):
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    ser_max, ser_min = ser.max(), ser.min()
+    init_logger().info("min/max: {min}/{max}".format(min=ser_min, max=ser_max))
+    delta = (ser_max - ser_min) * d
+    ser.plot(kind=kind, ylim=(ser_min - delta, ser_max + delta), title=title)
+    plt.show()
+
+
 def draw_hist(s, h_type: str = "dist", name: str = None):
     h_method = {
         "dist": sns.distplot,
