59be9f441a28456322e12d97fbaf594d588d521b
HANK <hankpkj@gmail.com>
2020-05-01 04:27:51 +0900

    HANK: rename: pandas.py -> get_df.py & create data_injection.py

---
+++ src/main/python/util/data_injection.py
@@ -0,0 +1,56 @@
+import urllib.request
+import json
+import pandas as pd
+from pandas.io.json import json_normalize
+import requests
+import datetime
+
+#-*- coding:utf-8 -*-
+
+"""
+def main():
+    date = '20200420'
+    service_key = 'rdmH5XR5ycW9emHC9D56bNydZrvc6546Z0fxlqCdCPc4RpWZ99GMoGoRIKRabGJKz2WFcTwN9ekSTfCvzzhiYA=='
+    url='http://apis.data.go.kr/B552895/openapi/service/OrgPriceExaminService/getExaminPriceList?\
+    ServiceKey='+service_key+'&type=json&pageNo=1&numOfRows=10&examinDe='+date+'&examinCd=6&prdlstCd=223'
+
+    response = urllib.request.urlopen(url)
+
+    json_str = response.read().decode("utf-8")
+
+    json_object = json.loads(json_str)
+    # print(json_object)
+    # body = [json_object['body']['item']]
+
+    df = json_normalize(json_object['body']['item'])
+    print(df)
+"""
+def main():
+    today = datetime.date.today()
+    start_dt = today - datetime.timedelta(days=100)
+    today = today.strftime('%Y%m%d')
+    start_dt = start_dt.strftime('%Y%m%d')
+
+    args= {
+        'numberOfRows': 10,
+        'pageNo': 1,
+        'ServiceKey': "",
+        'examinDe': today,
+    }
+
+    args_str = ""
+    for k, v in args.items():
+        args_str += '%s=%s' % (k, v)
+
+    res = requests.get('http://apis.data.go.kr/B552895/openapi/service/OrgPriceExaminService/getExaminPriceList?returnType=json%s' % args_str )
+    data = res.json()
+    items = data['response']['body']['items']
+    df_items = pd.DataFrame(items)
+    df_csv = df_items.to_csv('json.csv', encoding='utf-8-sig')
+    print(items)
+
+    # TODO : save df_csv
+
+
+if __name__ == '__main__':
+    main()
