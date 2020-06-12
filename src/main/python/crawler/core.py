271d1b80144b2576d5e131a96b012904f4702586
meowpunch <jinhoonbae@gmail.com>
2020-06-12 16:43:51 +0900

    MEOW: init RecipeCralwer, TODO: mapping & storing recipe

---
+++ src/main/python/crawler/core.py
@@ -0,0 +1,15 @@
+class Crawler:
+    def __init__(self):
+        pass
+
+    def process(self):
+        pass
+
+
+class RecipeCrawler(Crawler):
+    def __init__(self, url):
+        self.url = url
+        super().__init__()
+
+    def process(self):
+        pass
