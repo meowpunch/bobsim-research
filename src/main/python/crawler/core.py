7e4c716c6db28094a05d656942b5b3e40fc2e052
meowpunch <jinhoonbae@gmail.com>
2020-06-16 00:53:47 +0900

    MEOW: refactoring inheritance structure

--- src/main/python/crawler/core.py
+++ src/main/python/crawler/core.py
@@ -9,9 +9,10 @@ from util.s3_manager.manage import S3Manager
 
 
 class RecipeCrawler:
-    def __init__(self, base_url, candidate_num):
+    def __init__(self, base_url, candidate_num, field, bucket_name, key):
         self.logger = init_logger()
-        self.s3_manager = S3Manager(bucket_name="production-bobsim")
+        self.s3_manager = S3Manager(bucket_name=bucket_name)
+        self.key = key
 
         self.chrome_path = "C:/chromedriver"
         self.driver = webdriver.Chrome(self.chrome_path)
@@ -19,6 +20,8 @@ class RecipeCrawler:
 
         self.base_url = base_url
         self.candidate_num = candidate_num
+
+        self.field = field
 
     def process(self):
         result_code = list(map(lambda n: self.crawl_recipe(recipe_num=n), self.candidate_num))
@@ -72,7 +75,8 @@ class RecipeCrawler:
         self.logger.debug("success to connect with '{url}'".format(url=target_url))
 
     def save(self, recipe: dict, recipe_num=6847470):
-        self.s3_manager.save_dict_to_json(data=recipe, key="crawled_recipe/{num}.json".format(num=recipe_num))
+        self.s3_manager.save_dict_to_json(data=recipe,
+                                          key="{prefix}/{num}.json".format(prefix=self.key, num=recipe_num))
 
     def get_recipe(self):
         """
@@ -82,7 +86,8 @@ class RecipeCrawler:
 
 
 class MangeCrawler(RecipeCrawler):
-    def __init__(self):
+    def __init__(self, base_url="https://www.10000recipe.com/recipe/recipe_num", candidate_num=range(6828809, 6828811), field=None,
+                 bucket_name="production-bobsim", key="crawled_recipe/mange"):
         """
             https://www.10000recipe.com/recipe/recipe_num
             recipe_num: about 6828805 ~ 6935000
@@ -91,12 +96,16 @@ class MangeCrawler(RecipeCrawler):
                             total_recipe
                 15.06.2020: 138,873
         """
-        self.field = ['title', 'description', 'views', 'time', 'person', 'difficulty',
-                      'items', 'steps', 'caution', 'writer', 'comments', 'tag']
+        if field is None:
+            field = ['title', 'description', 'views', 'time', 'person', 'difficulty',
+                     'items', 'steps', 'caution', 'writer', 'comments', 'tag']
 
         super().__init__(
-            base_url="https://www.10000recipe.com/recipe/",
-            candidate_num=range(6828809, 6828811),
+            base_url=base_url,
+            candidate_num=candidate_num,
+            field=field,
+            bucket_name=bucket_name,
+            key=key
         )
 
     def get_recipe(self):
@@ -135,9 +144,6 @@ class MangeCrawler(RecipeCrawler):
             "comments": lambda d: d.find_element_by_class_name("view_reply").text.split("\n"),
             "tag": lambda d: d.find_element_by_class_name("view_tag").text.split("#"),
         }[key](self.driver)
-
-    def save(self, recipe: dict, recipe_num=6847470):
-        self.s3_manager.save_dict_to_json(data=recipe, key="crawled_recipe/mange/{num}.json".format(num=recipe_num))
 
 
 class HaemukCrawler(RecipeCrawler):
