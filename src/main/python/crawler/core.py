c89efec66a27a575e4b506bfd2e1a7de6eaf641a
meowpunch <jinhoonbae@gmail.com>
2020-06-15 14:21:25 +0900

    MEOW: Mange Haemuk Crawler

--- src/main/python/crawler/core.py
+++ src/main/python/crawler/core.py
@@ -1,15 +1,46 @@
-class Crawler:
+from selenium import webdriver
+
+
+class RecipeCrawler:
     def __init__(self):
+        pass
+
+    def connection(self):
         pass
 
     def process(self):
         pass
 
 
-class RecipeCrawler(Crawler):
-    def __init__(self, url):
-        self.url = url
+class MangeCrawler(RecipeCrawler):
+    def __init__(self, url="https://haemukja.com/recipes/"):
+        self.base_url = url
+        self.chrome_path = "C:/chromedriver"
         super().__init__()
+
+    @property
+    def driver(self):
+        return webdriver.Chrome(self.chrome_path)
+
+    def connection(self):
+        self.driver.get(self.base_url)
+
+    def process(self):
+        """
+            1. connection
+            2. get html source
+        :return:
+        """
+        self.connection()
+        a = self.driver.find_elements_by_class_name("difficulty")
+        for e in a:
+            print(e.text)
+            e.click()
+
+
+class HaemukCrawler:
+    def __init__(self):
+        pass
 
     def process(self):
         pass
