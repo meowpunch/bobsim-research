86a00d40356f55de621a6752ab9d8f6e29c65562
meowpunch <jinhoonbae@gmail.com>
2020-06-21 19:32:50 +0900

    MEOW: GET crawl item Emart

--- src/main/python/api/app.py
+++ src/main/python/api/app.py
@@ -16,13 +16,21 @@ def main():
     # TODO: classify crawl recipe API service
     @app.route('/', methods=['GET'])
     def index():
-        return "<h3>crawling service</h3>\
+        return "<h2>Crawling Service</h2>\
+                <h3>Recipe</h3>\
                 <strong>Mangae</strong><br>\
                 [GET] : /crawl_recipe/M?str_num=6932924&end_num=6932926<br>\
                 [GET] : /recipe/M<br><br>\
                 <strong>Haemuk</strong><br>\
                 [GET] : /crawl_recipe/H?str_num=5004&end_num=5005<br>\
-                [GET] : /recipe/H<br><br>"
+                [GET] : /recipe/H<br><br>\
+                <h3>Item</h3>\
+                <strong>Emart</strong><br>\
+                [GET] : /crawl_item/E<br>\
+                [GET] : /item/E<br><br>\
+                <strong>Haemuk</strong><br>\
+                [GET] : /crawl_item/H<br>\
+                [GET] : /item/H<br><br>"
 
     @app.route('/crawl_recipe/<source>', methods=['GET'])
     def crawl_recipe(source):
@@ -37,23 +45,27 @@ def main():
             str_num, end_num = 6934386, 6934390
 
         logger.info("let's crawl {str} ~ {end} {source} recipes".format(str=str_num, end=end_num, source=source))
+
+        bucket_name = "production-bobsim"
+        key = "crawled_recipe/{s}".format(s=source)
+        candidate_num = range(int(str_num), int(end_num))
         field = ['title', 'items', "duration", "tags"]
 
         if source == "M":
             result = MangaeRecipeCrawler(
                 base_url="https://www.10000recipe.com/recipe",
-                candidate_num=range(int(str_num), int(end_num)),
+                candidate_num=candidate_num,
                 field=field,
-                bucket_name="production-bobsim",
-                key="crawled_recipe/{s}".format(s=source)
+                bucket_name=bucket_name,
+                key=key
             ).process()
         elif source == "H":
             result = HaemukRecipeCrawler(
                 base_url="https://www.haemukja.com/recipes",
-                candidate_num=range(int(str_num), int(end_num)),
+                candidate_num=candidate_num,
                 field=field,
-                bucket_name="production-bobsim",
-                key="crawled_recipe/{s}".format(s=source)
+                bucket_name=bucket_name,
+                key=key
             ).process()
         else:
             raise NotImplementedError
@@ -72,12 +84,25 @@ def main():
         """
         :return: jsonified recipe
         """
+        logger.info("let's crawl item categories {source} recipes".format(source=source))
+
+        bucket_name = "production-bobsim"
+        key = "crawled_recipe/{s}".format(s=source)
+        head = True
+
         if source == "H":
             result = HaemukItemCrawler(
                 base_url="https://www.haemukja.com/refrigerator",
-                bucket_name="production-bobsim",
-                key="crawled_item/{s}".format(s=source),
-                head=True
+                bucket_name=bucket_name,
+                key=key,
+                head=head
+            ).process()
+        elif source == "E":
+            result = HaemukItemCrawler(
+                base_url="http://emart.ssg.com/",
+                bucket_name=bucket_name,
+                key=key,
+                head=head
             ).process()
         else:
             raise NotImplementedError
